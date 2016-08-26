ea7ed905b11fc94120c64b8bcc6110983ed16d1e
Daniel Chen <team6612@gmail.com>
2016-08-26 13:48:41 +0800

    build an example from socket.io

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/static'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
